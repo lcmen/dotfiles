acb11f131816833fbd8620a21d0af3a72176080e
Lucas Mendelowski <lucas@mendelowski.com>
2021-11-15 21:38:08 +0100

    Add lsp configuration for Neovim

--- dot_config/nvim/lua/packages.lua
+++ dot_config/nvim/lua/packages.lua
@@ -3,7 +3,25 @@
 -----------------------------------------------------------
 local call = vim.call
 local g = vim.g
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true }
 local Plug = vim.fn['plug#']
+
+-----------------------------------------------------------
+-- Functions
+-----------------------------------------------------------
+local on_attach = function(client, bufnr)
+    ion(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    map(bufnr, 'n', 'g0', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
+    map(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    map(bufnr, 'n', 'gf', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    map(bufnr, 'n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    map(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    map(bufnr, 'n', 'g=',  '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    map(bufnr, 'n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    map(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    map(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+end
 
 -----------------------------------------------------------
 -- External packages
@@ -16,8 +34,11 @@ Plug('airblade/vim-gitgutter')
 Plug('christoomey/vim-tmux-navigator')
 Plug('ctrlpvim/ctrlp.vim')
 Plug('docunext/closetag.vim')
+Plug('hrsh7th/nvim-compe')
 Plug('janko-m/vim-test')
 Plug('lokikl/vim-ctrlp-ag')
+Plug('kabouzeid/nvim-lspinstall')
+Plug('neovim/nvim-lspconfig')
 Plug('rking/ag.vim')
 Plug('ryanoasis/vim-devicons')
 Plug('scrooloose/nerdtree')
@@ -38,3 +59,8 @@ call('plug#end')
 -- Packages settings
 -----------------------------------------------------------
 g.NERDTreeShowHidden = 1
+
+require'lspinstall'.setup()
+for _, server in pairs(require'lspinstall'.installed_servers()) do
+    require'lspconfig'[server].setup{ on_attach = on_attach }
+end
