861ca85268878d4b948c8a97b7f1e36288b0489f
Lucas Mendelowski <lucas@mendelowski.com>
2021-11-16 14:42:52 +0100

    Configure autocomplete for Neovim

--- dot_config/nvim/lua/packages.lua
+++ dot_config/nvim/lua/packages.lua
@@ -3,8 +3,7 @@
 -----------------------------------------------------------
 local call = vim.call
 local g = vim.g
-local map = vim.api.nvim_buf_set_keymap
-local ion = vim.api.nvim_buf_set_option
+local lsp = vim.lsp
 local opts = { noremap = true }
 local Plug = vim.fn['plug#']
 
@@ -12,14 +11,18 @@ local Plug = vim.fn['plug#']
 -- Functions
 -----------------------------------------------------------
 local on_attach = function(client, bufnr)
+    local map = vim.api.nvim_buf_set_keymap
+    local ion = vim.api.nvim_buf_set_option
+
     ion(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
     map(bufnr, 'n', 'g0', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
     map(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
     map(bufnr, 'n', 'gf', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
     map(bufnr, 'n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
     map(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    map(bufnr, 'n', 'g=',  '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-    map(bufnr, 'n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    map(bufnr, 'n', 'g=', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    map(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    map(bufnr, 'n', 'L', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
     map(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
     map(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
 end
@@ -60,7 +63,26 @@ call('plug#end')
 -----------------------------------------------------------
 g.NERDTreeShowHidden = 1
 
+require'compe'.setup({
+    enabled = true;
+    autocomplete = true;
+    min_length = 1;
+    source = {
+        path = true;
+        buffer = true;
+        calc = true;
+        nvim_lsp = true;
+    };
+})
+
 require'lspinstall'.setup()
 for _, server in pairs(require'lspinstall'.installed_servers()) do
     require'lspconfig'[server].setup{ on_attach = on_attach }
 end
+
+lsp.handlers['textDocument/publishDiagnostics'] = lsp.with(lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = false,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+})
