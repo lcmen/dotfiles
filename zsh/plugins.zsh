00042fc5534eef1dfd220b4e46eb91d6cd44b0e1
Lucas Mendelowski <lucas@mendelowski.com>
2025-03-23 20:06:53 +0100

    Switch from asdf to mise

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,11 +1,11 @@
 # Load Homebrew first so other plugins relying `brew`, `brew --prefix`, etc. can work
 source "$ZDOTDIR/plugins/brew.zsh"
 
-source "$ZDOTDIR/plugins/asdf.zsh"
 source "$ZDOTDIR/plugins/bd.zsh"
 source "$ZDOTDIR/plugins/dirs.zsh"
 source "$ZDOTDIR/plugins/docker.zsh"
 source "$ZDOTDIR/plugins/elixir.zsh"
 source "$ZDOTDIR/plugins/fzf.zsh"
+source "$ZDOTDIR/plugins/mise.zsh"
 source "$ZDOTDIR/plugins/ruby.zsh"
 source "$ZDOTDIR/plugins/tmux.zsh"
