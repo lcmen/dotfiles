c844f57c78b4f08a98c4dc184e4d76e1053bc1fa
Lucas Mendelowski <lucas@mendelowski.com>
2025-07-14 07:46:24 +0200

    Update provisioning and mise config

--- provision.sh
+++ provision.sh
@@ -7,36 +7,33 @@ echo "Brewing your Mac..."
 brew tap chipmk/tap
 brew tap lcmen/extra
 
-HOMEBREW_NO_VERIFY_ATTESTATIONS=1 brew install -q bat \
-                                                  docker-mac-net-connect \
-                                                  fzf \
-                                                  git \
-                                                  libpq \
-                                                  neovim_bin \
-                                                  redis \
-                                                  ripgrep \
-                                                  shellcheck \
-                                                  stow \
-                                                  tig \
-                                                  tmux
+brew install -q bat \
+                docker-mac-net-connect \
+                fzf \
+                git \
+                libpq \
+                libyaml \
+                neovim_bin \
+                ripgrep \
+                shellcheck \
+                stow \
+                tig \
+                tmux
 
-HOMEBREW_NO_VERIFY_ATTESTATIONS=1 brew install --cask -q alacritty \
-                                                         android-file-transfer \
-                                                         appcleaner \
-                                                         cursor \
-                                                         firefox \
-                                                         font-fira-code-nerd-font \
-                                                         google-chrome \
-                                                         microsoft-teams \
-                                                         quicklook-csv \
-                                                         quicklook-json \
-                                                         postman \
-                                                         pocket-casts \
-                                                         the-unarchiver \
-                                                         whatsapp \
-                                                         vlc
+brew install --cask -q appcleaner \
+                       docker \
+                       firefox \
+                       font-fira-code-nerd-font \
+                       google-chrome \
+                       microsoft-teams \
+                       quicklook-csv \
+                       quicklook-json \
+                       postman \
+                       pocket-casts \
+                       the-unarchiver \
+                       visual-studio-code
 
-HOMEBREW_NO_VERIFY_ATTESTATIONS=1 brew upgrade --greedy
+brew upgrade --greedy
 
 sudo brew services start docker-mac-net-connect
 
