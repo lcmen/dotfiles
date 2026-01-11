798fb552e636411f1d5fc9d547b9b2eb30244293
Lucas Mendelowski <lucas@mendelowski.com>
2026-01-11 19:29:08 +0100

    Update Linux packages

--- linux.sh
+++ linux.sh
@@ -30,6 +30,7 @@ setup_ubuntu() {
     echo "Installing packages..."
     sudo apt-get update
     sudo apt-get install -y \
+        build-essential \
         ca-certificates \
         bat \
         fish \
