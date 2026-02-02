d35296618c2b630aa19da07015e5c9769ed17a6b
Lucas Mendelowski <lucas@mendelowski.com>
2026-02-02 19:58:15 +0100

    Update paths for Fish

--- fish/config.fish
+++ fish/config.fish
@@ -10,7 +10,8 @@ set -gx PGPASSFILE "$XDG_CONFIG_HOME/psql/pgpass"
 set -gx RIPGREP_CONFIG_PATH "$XDG_CONFIG_HOME/ripgrep/config"
 
 # PATHs
-fish_add_path "./node_modules/.bin"  # Local node_modules binaries
 fish_add_path "$HOME/.bin"           # Local binaries
+fish_add_path "$HOME/.local/bin"     # Local binaries
+fish_add_path "./node_modules/.bin"  # Local node_modules binaries
 fish_add_path "/usr/local/bin"       # Homebrew binaries (if installed)
 fish_add_path "/usr/local/sbin"      # Homebrew binaries (if installed)
