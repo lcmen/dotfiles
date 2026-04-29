28b8aed3236eeb4c452a89d5e30a9e1db973ddd4
Lucas Mendelowski <lucas@mendelowski.com>
2026-04-29 16:21:07 +0200

    Improve gw script

--- fish/functions/gw.fish
+++ fish/functions/gw.fish
@@ -1,6 +1,12 @@
 function gw --description "Git worktree wrapper"
-  command gw $argv
-  and if test "$argv[1]" = remove
-    exit
+  set -l output (command gw $argv)
+  set -l status_code $status
+
+  if test (count $output) -eq 1; and test -d "$output[1]"
+    cd "$output[1]"
+  else if test (count $output) -gt 0
+    printf "%s\n" $output
   end
+
+  return $status_code
 end
